705bebb2bf25c8cb10d2666ef0a15c2ff57a707e
EXisTAnZ <ozim_ssspam@mail.ru>
2022-09-28 01:13:42 +0300

    feat: added onclick to logotype 	modified:   online-zoo/pages/donate/donate.html 	new file:   online-zoo/pages/donate/donate.js 	deleted:    online-zoo/pages/donate/script.js 	modified:   online-zoo/pages/main/index.html 	new file:   online-zoo/pages/main/index.js

---
+++ online-zoo/pages/donate/donate.js
@@ -0,0 +1,7 @@
+const logo = document.querySelectorAll('.pet-logo')
+logo.forEach(el => {
+    console.log('interactive')
+    el.addEventListener('click', () => {
+        document.location.href = "index.html"
+    })
+})
