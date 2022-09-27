4cc2faaae1c8d855d7a5de9ea69551be35888599
EXisTAnZ <ozim_ssspam@mail.ru>
2022-09-27 08:42:40 +0300

    feat: started donate.html 	new file:   online-zoo/pages/donate/donate.css 	new file:   online-zoo/pages/donate/donate.html 	new file:   online-zoo/pages/donate/script.js 	modified:   online-zoo/pages/main/style.css

---
+++ online-zoo/pages/donate/script.js
@@ -0,0 +1,13 @@
+a = true, b = true;
+
+console.log(a && b)
+
+console.log(!a && b)
+
+    console.log(a && !b)
+
+        console.log(!a && !b)
+
+            console.log(!(a && b))
+
+            console.log(7 * 3 + (4 / 2) - 8 + 2 - 1)
