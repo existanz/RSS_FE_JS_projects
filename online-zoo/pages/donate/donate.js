12e6ccdd67e98845fa86a6e9bb60066da1d9913f
EXisTAnZ <ozim_ssspam@mail.ru>
2022-10-05 13:17:34 +0300

    fix: finished layout for 1000px screen 	modified:   online-zoo/assets/css/style.css 	new file:   online-zoo/assets/images/panda_bg_sd.png 	new file:   online-zoo/pages/donate/adaptive.css 	modified:   online-zoo/pages/donate/donate.css 	modified:   online-zoo/pages/donate/donate.html 	modified:   online-zoo/pages/donate/donate.js 	modified:   online-zoo/pages/main/adaptive.css

--- online-zoo/pages/donate/donate.js
+++ online-zoo/pages/donate/donate.js
@@ -5,3 +5,8 @@ logo.forEach(el => {
         document.location.href = "../main/index.html"
     })
 })
+console.log(screen.width);
+if (screen.width<1440) {
+    console.log(document.querySelector(".feed-slider-range"));
+    document.querySelector(".feed-slider-range").setAttribute("min","1");
+}
