616da2aeb4077c9d7c0a9a33479f8fc808d4392e
EXisTAnZ <ozim_ssspam@mail.ru>
2022-10-10 22:35:59 +0300

    feat: added range slider logic for donate page 	modified:   online-zoo/pages/donate/adaptive.css 	modified:   online-zoo/pages/donate/donate.html 	modified:   online-zoo/pages/donate/donate.js 	modified:   online-zoo/pages/main/adaptive.css

--- online-zoo/pages/donate/donate.js
+++ online-zoo/pages/donate/donate.js
@@ -1,12 +1,33 @@
-const logo = document.querySelectorAll('.pet-logo')
+const logo = document.querySelectorAll(".pet-logo"),
+rangeSlider = document.querySelector(".feed-slider-range") 
+
 logo.forEach(el => {
-    console.log('interactive')
     el.addEventListener('click', () => {
         document.location.href = "../main/index.html"
     })
 })
-console.log(screen.width);
-if (screen.width<1440) {
-    console.log(document.querySelector(".feed-slider-range"));
-    document.querySelector(".feed-slider-range").setAttribute("min","1");
+
+const setRangeMin = () => {
+    const screenWidth = screen.width;
+    console.log(screenWidth);
+    if (screenWidth>1440) rangeSlider.setAttribute("min","0")
+    else if (screenWidth<=1440 && screenWidth>980) rangeSlider.setAttribute("min","1")
+    else if (screenWidth<=980) rangeSlider.setAttribute("min","2");
 }
+
+setRangeMin();
+window.addEventListener('resize', setRangeMin);
+
+const changeRange = () => {
+    console.log(rangeSlider.value);
+    const rangeTexts = document.querySelectorAll(".feed-slider-itemText"),
+    amountInput = document.querySelector(".feed-friend-input"),
+    arrValues = [5000, 2000, 1000, 500, 250, 100, 50, 25]
+    rangeTexts.forEach((el,id) => {
+            el.classList.remove("feed-slider-item-active");
+            if (id==rangeSlider.value) el.classList.add("feed-slider-item-active");
+    })
+    amountInput.value = arrValues[rangeSlider.value];
+
+}
+rangeSlider.addEventListener('change', changeRange);
