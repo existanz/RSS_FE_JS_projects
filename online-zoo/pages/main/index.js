a2077cef9c82f55452970f8c595a7afdd9c86292
EXisTAnZ <ozim_ssspam@mail.ru>
2022-10-11 13:22:53 +0300

    feat: added testimonials slider 	modified:   online-zoo/pages/main/adaptive.css 	modified:   online-zoo/pages/main/index.css 	modified:   online-zoo/pages/main/index.html 	modified:   online-zoo/pages/main/index.js

--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -1,7 +1,19 @@
 const logo = document.querySelectorAll('.pet-logo')
 logo.forEach(el => {
-    console.log('interactive')
     el.addEventListener('click', () => {
         document.location.href = "index.html"
     })
 })
+
+//TESTIMONIALS SLIDER
+const testCards = document.querySelector('.testimonials-cards');
+const inputRange = document.querySelector('.range-slider');
+let cWidth = 296;
+if (screen.width < 1440) cWidth = 323;
+window.addEventListener('resize', () => {
+    cWidth = 296;
+if (screen.width < 1440) cWidth = 323;
+})
+ inputRange.addEventListener('input', () => {
+    testCards.style.left = -inputRange.value*cWidth + 'px';
+ });
