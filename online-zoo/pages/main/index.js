354d9b96fdcd3b37545b47ac0b77f1ef8318953a
EXisTAnZ <ozim_ssspam@mail.ru>
2022-10-13 15:25:18 +0300

    feat: added slider for animals 	new file:   online-zoo/assets/images/pets/alligator.png 	new file:   online-zoo/assets/images/pets/armadillo.png 	new file:   online-zoo/assets/images/pets/chacoan-peccary.png 	new file:   online-zoo/assets/images/pets/cheetahs.png 	new file:   online-zoo/assets/images/pets/eagle.png 	new file:   online-zoo/assets/images/pets/elephant.png 	new file:   online-zoo/assets/images/pets/giraffe.png 	new file:   online-zoo/assets/images/pets/gorilla.png 	new file:   online-zoo/assets/images/pets/heena.png 	new file:   online-zoo/assets/images/pets/koala.png 	new file:   online-zoo/assets/images/pets/pandas.png 	new file:   online-zoo/assets/images/pets/penguin.png 	new file:   online-zoo/assets/images/pets/platypus.png 	new file:   online-zoo/assets/images/pets/quokka.png 	new file:   online-zoo/assets/images/pets/sleepy.png 	new file:   online-zoo/assets/images/pets/sloth.png 	new file:   online-zoo/assets/images/pets/tryceratops.png 	new file:   online-zoo/assets/images/pets/turtle.png 	modified:   online-zoo/pages/main/adaptive.css 	modified:   online-zoo/pages/main/index.css 	modified:   online-zoo/pages/main/index.html 	modified:   online-zoo/pages/main/index.js

--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -1,3 +1,248 @@
+const arrPets = [
+    {
+      img: "../../assets/images/pets/pandas.png",
+      title: "Giant pandas",
+      location: "Native to Southwest China",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/eagle.png",
+      title: "Eagles",
+      location: "Native to South America",
+      food: "../../assets/icons/meatfish.svg",
+    },
+    {
+      img: "../../assets/images/pets/gorilla.png",
+      title: "Gorillas",
+      location: "Native to Congo",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/sloth.png",
+      title: "Two-toed sloth",
+      location: "Mesoamerica, South America",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/cheetahs.png",
+      title: "Cheetahs",
+      location: "Native to Africa",
+      food: "../../assets/icons/meatfish.svg",
+    },
+    {
+      img: "../../assets/images/pets/penguin.png",
+      title: "Penguins",
+      location: "Native to Antarctica",
+      food: "../../assets/icons/meatfish.svg",
+    },
+    {
+      img: "../../assets/images/pets/alligator.png",
+      title: "Alligators",
+      location: "Native to Southeastern U. S.",
+      food: "../../assets/icons/meatfish.svg",
+    },
+    {
+      img: "../../assets/images/pets/armadillo.png",
+      title: "Armadillo",
+      location: "Native to America",
+      food: "../../assets/icons/meatfish.svg",
+    },
+    {
+      img: "../../assets/images/pets/turtle.png",
+      title: "Arakan forest turtle",
+      location: "Native to Arakan Hills",
+      food: "../../assets/icons/meatfish.svg",
+    },
+    {
+      img: "../../assets/images/pets/quokka.png",
+      title: "Quokkas",
+      location: "Native to Rottnest Island",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/sleepy.png",
+      title: "Sleepy",
+      location: "Native to Eastern Himalayas",
+      food: "../../assets/icons/meatfish.svg",
+    },
+    {
+      img: "../../assets/images/pets/tryceratops.png",
+      title: "Tryceratops",
+      location: "Native to Pangea",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/giraffe.png",
+      title: "Giraffe",
+      location: "Native to Africa",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/koala.png",
+      title: "Koala",
+      location: "Native to South Australia",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/elephant.png",
+      title: "Elephant",
+      location: "Native to Africa",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/chacoan-peccary.png",
+      title: "Chacoan peccary",
+      location: "Native to Gran Chaco",
+      food: "../../assets/icons/vegetables.svg",
+    },
+    {
+      img: "../../assets/images/pets/platypus.png",
+      title: "Platypus",
+      location: "Native to Australia",
+      food: "../../assets/icons/meatfish.svg",
+    },
+    {
+      img: "../../assets/images/pets/heena.png",
+      title: "Heena",
+      location: "Native to Africa",
+      food: "../../assets/icons/meatfish.svg",
+    },
+];
+
+//PETS CAROUSEL
+
+const animalLeft = document.querySelector(".animal-left"),
+animalCenter = document.querySelector(".animal-center"),
+animalRight = document.querySelector(".animal-right"),
+animalLLeft = document.querySelector(".animal-left-left"),
+animalRRight = document.querySelector(".animal-right-right"),
+animalWraper = document.querySelector(".animals-wraper"),
+nextBtn = document.querySelector(".arrow-right"),
+prevBtn = document.querySelector(".arrow-left")
+
+let slideWidth = -1550
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    console.log('shuffled')
+}
+function getPets(data) {
+    data.slice(0, 6).forEach(({img, title, location, food}) => {
+      const petsCardLeft = document.createElement("article");
+      petsCardLeft.classList.add("animal-card");
+  
+      petsCardLeft.innerHTML = `
+        <img
+          src="${img}"
+          alt="${title}"
+          class="pet-img"
+        />
+        <div class="animal-card-desc">
+          <div class="animal-card-text">
+            <h3 class="animal-card-title">${title}</h3>
+            <p class="animal-card-area">${location}</p>
+          </div>
+          <img 
+            src="${food}"
+            alt="" 
+            class="card-icon"
+          />
+        </div>
+      `;
+      animalLeft.append(petsCardLeft);
+      animalRRight.append(petsCardLeft.cloneNode());
+    });
+  
+    data.slice(6, 12).forEach(({img, title, location, food}) => {
+      const petsCenter = animalCenter;
+      const petsCardCenter = document.createElement("article");
+      petsCardCenter.classList.add("animal-card");
+  
+      petsCardCenter.innerHTML = `
+        <img
+          src="${img}"
+          alt="${title}"
+          class="pet-img"
+        />
+        <div class="animal-card-desc">
+          <div class="animal-card-text">
+            <h3 class="animal-card-title">${title}</h3>
+            <p class="animal-card-area">${location}</p>
+          </div>
+          <img 
+            src="${food}"
+            alt="" 
+            class="card-icon"
+          />
+        </div>
+      `;
+      petsCenter.append(petsCardCenter);
+    });
+  
+    data.slice(12).forEach(({img, title, location, food}) => {
+      const petsCardRight = document.createElement("article");
+      petsCardRight.classList.add("animal-card");
+  
+      petsCardRight.innerHTML = `
+        <img
+          src="${img}"
+          alt="${title}"
+          class="pet-img"
+        />
+        <div class="animal-card-desc">
+          <div class="animal-card-text">
+            <h3 class="animal-card-title">${title}</h3>
+            <p class="animal-card-area">${location}</p>
+          </div>
+          <img 
+            src="${food}"
+            alt="" 
+            class="card-icon"
+          />
+        </div>
+      `;
+      animalRight.append(petsCardRight);
+      animalLLeft.append(petsCardRight.cloneNode());
+    });
+}
+shuffle(arrPets);
+getPets(arrPets);
+
+function nextSlide() {
+    if (window.innerWidth >= 1600) {
+        slideWidth -= 1550
+        animalWraper.style.transform = `translateX(${slideWidth}px)`;
+        shuffle(arrPets);
+        getPets(arrPets);
+    } else if (window.innerWidth < 1600 && window.innerWidth >= 1000) {
+
+    } else if (window.innerWidth < 1000 && window.innerWidth >= 640) {
+   
+    }
+    nextBtn.disabled = true;
+    prevBtn.disabled = true;
+  }
+  
+  function prevSlide() {
+    if (window.innerWidth >= 1600) {
+        slideWidth += 1550;
+        animalWraper.style.transform = `translateX(${slideWidth}px)`;
+    } else if (window.innerWidth < 1600 && window.innerWidth >= 1000) {
+
+    } else if (window.innerWidth < 1000 && window.innerWidth >= 640) {
+
+    }
+    nextBtn.disabled = true;
+    prevBtn.disabled = true;
+  }
+  
+  nextBtn.addEventListener("click", nextSlide);
+  prevBtn.addEventListener("click", prevSlide);
+
+  //LOGO LINK
 const logo = document.querySelectorAll('.pet-logo')
 logo.forEach(el => {
     el.addEventListener('click', () => {
