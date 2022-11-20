2acd894156f7679694079f42037ad0cc35ff258e
EXisTAnZ <ozim_ssspam@mail.ru>
2022-11-20 16:06:40 +0300

    feat: add random function

--- src/data/game.js
+++ src/data/game.js
@@ -2,3 +2,8 @@ import birdsData from "./birds";
 
 const curQuiz = document.querySelector('.cur-quiz');
 curQuiz.classList.add('hidden');
+const rand = (min, max) => {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+};
+console.log(rand(1,5));
