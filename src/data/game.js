14f9026a54375cda7ab740a78fa31efb0961a2f9
EXisTAnZ <ozim_ssspam@mail.ru>
2022-11-21 22:40:30 +0300

    feat: add question section to quiz 	modified:   src/assets/styles/game.css 	modified:   src/data/game.js 	modified:   src/game.html

--- src/data/game.js
+++ src/data/game.js
@@ -1,17 +1,41 @@
-import birdsData from "./birds";
+import birdsData from './birds';
+import emptyBird from '../assets/images/bird.jpg'
 
-let lang = 'en';
+let lang = 'ru';
 const curQuiz = document.querySelector('.cur-quiz');
 curQuiz.classList.add('hidden');
 const rand = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
-console.log(rand(1,5));
 
-let curStage = 0;
+let curStage = 0,
+    isPlay = false;
 const nextStage = () => curStage++;
+
+
 let quizBird = birdsData[curStage][rand(0,5)];
 const audioQuiz = new Audio();
 audioQuiz.src = quizBird.audio;
 console.log(quizBird.name[lang]);
 console.log(lang);
-audioQuiz.play();
+
+const setNewStage = () => {
+    const curImg = document.querySelector('.cur-quiz__img')
+    curImg.style.backgroundImage =  `url(${emptyBird})`;
+};
+
+setNewStage();
+
+const playButton = document.querySelector('.play');
+
+const playPauseAudio = () => {
+    if (!isPlay) {
+      isPlay = true;
+      playButton.classList.add('pause');
+      audioQuiz.play();
+    } else {
+      isPlay = false;
+      playButton.classList.remove('pause');
+      audioQuiz.pause();
+    }
+  };
+  playButton.addEventListener('click', playPauseAudio);
