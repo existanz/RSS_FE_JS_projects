48690fb3cb69e445aa6e2552c09f783bd7594e66
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-11-22 11:23:38 +0300

    feat: add list of birds to quiz

--- src/data/game.js
+++ src/data/game.js
@@ -2,8 +2,10 @@ import birdsData from './birds';
 import emptyBird from '../assets/images/bird.jpg'
 
 let lang = 'ru';
-const curQuiz = document.querySelector('.cur-quiz');
-curQuiz.classList.add('hidden');
+const curQuiz = document.querySelector('.cur-quiz'),
+      curTitle = document.querySelector('.cur-quiz__title'),
+      quizList = document.querySelector('.quiz__list')
+
 const rand = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
 
@@ -18,9 +20,19 @@ audioQuiz.src = quizBird.audio;
 console.log(quizBird.name[lang]);
 console.log(lang);
 
+const setQuizList = () => {
+  birdsData[curStage].forEach(el => {
+    const li = document.createElement('li');
+    li.classList.add('list__bird');
+    li.textContent = el.name[lang];
+    quizList.append(li);
+  });
+}
+
 const setNewStage = () => {
     const curImg = document.querySelector('.cur-quiz__img')
     curImg.style.backgroundImage =  `url(${emptyBird})`;
+    setQuizList();
 };
 
 setNewStage();
