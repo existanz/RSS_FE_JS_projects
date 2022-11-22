790354b900205b5334913ac3d56dc6549ed1ba87
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-11-22 16:36:23 +0300

    fix: move game.js to index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './index.html';
 import './game.html';
 import './gallery.html';
 import './data/birds';
-import './data/game';
 console.log("Its start!");
 let lang = 'en';
 
@@ -16,3 +15,67 @@ const changeLang = () => {
 };
 linkLang.addEventListener('click', changeLang);
 console.log(lang);
+
+
+/* GAME JS MOVE THERE because webpack*/
+import birdsData from './data/birds';
+import emptyBird from './assets/images/bird.jpg'
+
+const curQuiz = document.querySelector('.cur-quiz'),
+      curTitle = document.querySelector('.cur-quiz__title'),
+      quizList = document.querySelector('.birds__list')
+
+const rand = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+
+
+let curStage = 0,
+    isPlay = false;
+const nextStage = () => curStage++;
+
+
+let quizBird = birdsData[curStage][rand(0,5)];
+const audioQuiz = new Audio();
+audioQuiz.src = quizBird.audio;
+console.log(quizBird.name[lang]);
+console.log(lang);
+
+const showInfo = (el) => () => {
+  console.log(el.name[lang]);
+}
+
+const setQuizList = () => {
+  if (quizList) {
+    quizList.innerHTML = '';
+    birdsData[curStage].forEach(el => {
+      const li = document.createElement('li');
+      li.classList.add('list__item');
+      li.textContent = el.name[lang];
+      li.addEventListener('click', showInfo(el));
+      quizList.append(li);
+    });
+  }
+}
+
+const setNewStage = () => {
+    const curImg = document.querySelector('.cur-quiz__img');
+    if (curImg) curImg.style.backgroundImage =  `url(${emptyBird})`;
+    setQuizList();
+};
+
+setNewStage();
+
+const playButton = document.querySelector('.play');
+
+const playPauseAudio = () => {
+    if (!isPlay) {
+      isPlay = true;
+      playButton.classList.add('pause');
+      audioQuiz.play();
+    } else {
+      isPlay = false;
+      playButton.classList.remove('pause');
+      audioQuiz.pause();
+    }
+  };
+  if (playButton) playButton.addEventListener('click', playPauseAudio);
+/*END OF GAME JS*/
