1ce9c3a18fda5167e1fd15317aefda292be63913
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-13 13:45:15 +0300

    fix: replce img_placeholder in news.ts

--- migration-ts/src/components/view/news/news.ts
+++ migration-ts/src/components/view/news/news.ts
@@ -15,7 +15,7 @@ class News {
             if (idx % 2) (newsClone.querySelector('.news__item') as HTMLElement).classList.add('alt');
 
             (newsClone.querySelector('.news__meta-photo') as HTMLElement).style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
+                item.urlToImage || 'ihttps://mailchimp.com/static/images/404Horse.gif?w=780&fm=webp&q=80'
             })`;
             (newsClone.querySelector('.news__meta-author') as HTMLElement).textContent =
                 item.author || item.source.name;
