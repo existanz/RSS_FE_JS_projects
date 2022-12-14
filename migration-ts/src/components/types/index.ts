58610d86e8cd8c2d76c806f9802da95a6136b3ab
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-14 09:11:20 +0300

    fix: replace country and language enum by string

--- migration-ts/src/components/types/index.ts
+++ migration-ts/src/components/types/index.ts
@@ -8,85 +8,11 @@ enum Category {
     technology,
 }
 
-enum Language {
-    ar,
-    de,
-    en,
-    es,
-    fr,
-    he,
-    it,
-    nl,
-    no,
-    pt,
-    ru,
-    sv,
-    ud,
-    zh,
-}
-
-enum Country {
-    ae,
-    ar,
-    at,
-    au,
-    be,
-    bg,
-    br,
-    ca,
-    ch,
-    cn,
-    co,
-    cu,
-    cz,
-    de,
-    eg,
-    fr,
-    gb,
-    gr,
-    hk,
-    hu,
-    id,
-    ie,
-    il,
-    in,
-    it,
-    jp,
-    kr,
-    lt,
-    lv,
-    ma,
-    mx,
-    my,
-    ng,
-    nl,
-    no,
-    nz,
-    ph,
-    pl,
-    pt,
-    ro,
-    rs,
-    ru,
-    sa,
-    se,
-    sg,
-    si,
-    sk,
-    th,
-    tr,
-    tw,
-    ua,
-    us,
-    ve,
-    za,
-}
-
 interface Options {
     apiKey: string;
     category?: Category;
-    language?: Language;
-    country?: Country;
+    language?: string;
+    country?: string;
 }
 
 interface Source {
@@ -95,8 +21,8 @@ interface Source {
     description: string;
     url: string;
     category: Category;
-    language: Language;
-    country: Country;
+    language: string;
+    country: string;
 }
 
 interface Response {
