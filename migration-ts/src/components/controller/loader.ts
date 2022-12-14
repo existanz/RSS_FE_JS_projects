c94dd318906800cfd9aac57f66ce5978f7f12181
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-14 10:12:54 +0300

    fix: add access modifiers for Loader class

--- migration-ts/src/components/controller/loader.ts
+++ migration-ts/src/components/controller/loader.ts
@@ -8,7 +8,7 @@ class Loader {
         this.options = options;
     }
 
-    getResp(
+    public getResp(
         endpoint: Endpoint,
         options = {},
         callback = () => {
@@ -18,7 +18,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res: ErrorResp): ErrorResp {
+    public errorHandler(res: ErrorResp): ErrorResp {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -28,7 +28,7 @@ class Loader {
         return res;
     }
 
-    makeUrl(options = {}, endpoint: Endpoint) {
+    private makeUrl(options = {}, endpoint: Endpoint) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -39,7 +39,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method: Method, endpoint: Endpoint, callback: Callback<void>, options = {}) {
+    private load(method: Method, endpoint: Endpoint, callback: Callback<void>, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res: ErrorResp) => res.json())
