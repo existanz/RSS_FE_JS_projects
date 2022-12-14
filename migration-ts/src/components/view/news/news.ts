317fc44ee1d044717178512e91b7bea8a4285a8f
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-14 10:18:00 +0300

    fix: add access modifiers for News class

--- migration-ts/src/components/view/news/news.ts
+++ migration-ts/src/components/view/news/news.ts
@@ -2,7 +2,7 @@ import { Article } from '../../types/index';
 import './news.css';
 
 class News {
-    draw(data: Article[]) {
+    public draw(data: Article[]) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment = document.createDocumentFragment();
