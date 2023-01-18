7a8f5ce710dc8789890e0c1c201f99af5615ae30
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2023-01-18 11:39:14 +0300

    feat: add footer

---
+++ async-race/src/app/core/components/footer/footer.ts
@@ -0,0 +1,38 @@
+import './footer.scss';
+import DOMElement from '../../../shared/components/base-elements/dom-element';
+import LinkElement from '../../../shared/components/base-elements/link-element';
+import SVGicons from '../../../shared/components/svg-icons';
+
+export default class Footer extends DOMElement {
+  private container: DOMElement;
+
+  private footerLinx: DOMElement;
+
+  private year: DOMElement;
+
+  private footerSchoolLogo: DOMElement;
+
+  private gitHub: LinkElement;
+
+  private githubUser: DOMElement;
+
+  private rsschool: LinkElement;
+
+  constructor(parentNode: HTMLElement) {
+    super(parentNode, 'footer', ['footer']);
+
+    this.container = new DOMElement(this.node, 'div', ['container', 'footer__container']);
+
+    this.footerLinx = new DOMElement(this.container.node, 'div', ['footer-links']);
+
+    this.gitHub = new LinkElement(this.footerLinx.node, ['footer-links__item'], 'https://github.com/EXisTAnZ');
+    this.gitHub.node.innerHTML = SVGicons.github;
+    this.githubUser = new DOMElement(this.gitHub.node, 'div', ['footer-links__text'], 'Magomed Oziev');
+
+    this.year = new DOMElement(this.container.node, 'p', ['footer__year'], '2023');
+
+    this.footerSchoolLogo = new DOMElement(this.container.node, 'div', ['footer__shcool-logo']);
+    this.rsschool = new LinkElement(this.footerSchoolLogo.node, ['shcool-logo'], 'https://rs.school/');
+    this.rsschool.node.innerHTML = SVGicons.rsschool;
+  }
+}
--- async-race/src/app/shared/components/base-elements/link-element.ts
+++ async-race/src/app/shared/components/base-elements/link-element.ts
@@ -1,7 +1,7 @@
 import DOMElement from './dom-element';
 
 export default class LinkElement extends DOMElement {
-  constructor(parentNode: HTMLElement | null, classList: string[] = [], content = '', href = '') {
+  constructor(parentNode: HTMLElement | null, classList: string[] = [], href = '', content = '') {
     super(parentNode, 'a', classList, content);
 
     this.node.setAttribute('href', href);
