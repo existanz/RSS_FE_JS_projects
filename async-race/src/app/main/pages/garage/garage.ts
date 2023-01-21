0bc7639004148884ad8689b7d1fb3cc80fd4592e
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2023-01-21 18:20:52 +0300

    feat: add paginator

--- async-race/src/app/main/pages/garage/garage.ts
+++ async-race/src/app/main/pages/garage/garage.ts
@@ -5,25 +5,31 @@ import stateService from '../../../shared/services/state.service';
 import CarElement from '../../components/car-element';
 import GaragePanel from '../../components/garage-panel';
 import GarageTitle from '../../components/garage-title';
+import Paginator from '../../../shared/components/paginator';
 
 export default class GaragePage extends Page {
   private garagePanel: GaragePanel;
 
   private garageTitle: GarageTitle;
 
+  private paginator: Paginator;
+
   constructor() {
     super('garage');
     this.garagePanel = new GaragePanel(this.node);
     this.garageTitle = new GarageTitle(this.node, 0);
+    this.paginator = new Paginator(this.node, 40);
   }
 
   render() {
     this.node.innerHTML = '';
-    this.garagePanel = new GaragePanel(this.node);
-    this.garageTitle = new GarageTitle(this.node, 100);
+    this.node.append(this.garagePanel.node);
+    this.node.append(this.garageTitle.node);
     stateService.allData.forEach((item) => {
       const newCar = new CarElement(this.node, item);
       newCar.render();
     });
+    this.paginator.update(43);
+    this.node.append(this.paginator.node);
   }
 }
---
+++ async-race/src/app/shared/components/paginator.ts
@@ -0,0 +1,47 @@
+import { MyObject } from '../models/basse-types';
+import ButtonElement from './base-elements/button-element';
+import DOMElement from './base-elements/dom-element';
+
+export default class Paginator extends DOMElement {
+  public paginator: MyObject;
+
+  private totalCount: number;
+
+  private curPage: number;
+
+  private prevButton: ButtonElement;
+
+  private pageNum: DOMElement;
+
+  private nextButton: ButtonElement;
+
+  constructor(parentNode: HTMLElement, totalCount: number) {
+    super(parentNode, 'div', ['paginator']);
+
+    this.totalCount = totalCount;
+    this.paginator = { _limit: '7', _page: '1' };
+    this.curPage = parseInt(this.paginator._page);
+    this.prevButton = new ButtonElement(this.node, ['button', 'paginator__button', 'paginator__button-prev'], '<');
+    this.prevButton.node.addEventListener('click', () => {
+      if (!this.prevButton.disabled && this.curPage > 1) this.curPage--;
+      this.update(this.totalCount);
+    });
+    this.pageNum = new DOMElement(this.node, 'label', ['paginator__page-number'], this.paginator._page);
+    this.nextButton = new ButtonElement(this.node, ['button', 'paginator__button', 'paginator__button-next'], '>');
+    this.nextButton.node.addEventListener('click', () => {
+      if (!this.nextButton.disabled && this.curPage < Math.ceil(this.totalCount / parseInt(this.paginator._limit))) {
+        this.curPage++;
+        this.update(this.totalCount);
+      }
+    });
+    this.update(totalCount);
+  }
+
+  public update(totalCount: number) {
+    this.totalCount = totalCount;
+    this.paginator._page = this.curPage.toString();
+    this.pageNum.node.textContent = this.paginator._page;
+    this.prevButton.disabled = this.curPage == 1;
+    this.nextButton.disabled = totalCount < parseInt(this.paginator._limit) * this.curPage;
+  }
+}
