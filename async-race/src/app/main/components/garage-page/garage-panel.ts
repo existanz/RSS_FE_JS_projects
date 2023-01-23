ae5b433cf7325641120674834e2c03b6e4cf18c1
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2023-01-23 20:15:51 +0300

    feat: add name input filter and trasliter for ciryllic

--- async-race/src/app/main/components/garage-page/garage-panel.ts
+++ async-race/src/app/main/components/garage-page/garage-panel.ts
@@ -3,6 +3,7 @@ import DOMElement from '../../../shared/components/base-elements/dom-element';
 import InputElement from '../../../shared/components/base-elements/input-element';
 import stateService from '../../../shared/services/state.service';
 import garageApi from '../../services/api/garage-api';
+import GarageService from '../../services/garage-service';
 
 export default class GaragePanel extends DOMElement {
   private carPropPanel: DOMElement;
@@ -29,7 +30,9 @@ export default class GaragePanel extends DOMElement {
     this.carPropPanel = new DOMElement(this.node, 'div', ['garage__panel-car']);
     this.carNameInput = new InputElement(this.carPropPanel.node, ['input', 'input__car-name'], '');
     this.carNameInput.node.addEventListener('change', (ev) => {
-      this.carNameInput.value = (ev.target as HTMLInputElement).value;
+      this.carNameInput.value = GarageService.translitCarName(
+        (ev.target as HTMLInputElement).value.replace(/[^a-zа-яё0-9\s]/gi, '')
+      );
     });
     this.carColorInput = new InputElement(this.carPropPanel.node, ['input', 'input__car-color'], '');
     this.carColorInput.node.addEventListener('change', (ev) => {
@@ -42,15 +45,33 @@ export default class GaragePanel extends DOMElement {
         garageApi.createCar({ name: this.carNameInput.value, color: this.carColorInput.value });
     });
     this.updateButton = new ButtonElement(this.carPropPanel.node, ['button', 'button__update'], 'update');
+    this.updateButton.node.addEventListener('click', () => {
+      if (!this.updateButton.disabled) {
+        garageApi.updateCar({
+          name: this.carNameInput.value,
+          color: this.carColorInput.value,
+          id: stateService.selectedCar.id,
+        });
+      }
+    });
     this.racePanel = new DOMElement(this.node, 'div', ['garage__panel-race']);
     this.raceButton = new ButtonElement(this.racePanel.node, ['button', 'button__race'], 'race');
     this.resetButton = new ButtonElement(this.racePanel.node, ['button', 'button__reset'], 'reset');
     this.generateButton = new ButtonElement(this.racePanel.node, ['button', 'button__generate'], 'generate 100 cars');
-    this.update();
+    this.clear();
   }
 
-  public update() {
-    console.log(stateService.selectedCar.id);
+  public updateSel() {
+    console.log(stateService.selectedCar);
     this.updateButton.disabled = !stateService.selectedCar.id;
+    this.carNameInput.value = stateService.selectedCar.name;
+    this.carColorInput.value = stateService.selectedCar.color;
+  }
+
+  public clear() {
+    stateService.selectedCar.id = '0';
+    this.updateButton.disabled = true;
+    this.carNameInput.value = '';
+    this.carColorInput.value = '';
   }
 }
--- async-race/src/app/main/services/garage-service.ts
+++ async-race/src/app/main/services/garage-service.ts
@@ -1,4 +1,5 @@
 import { Car } from '../../shared/models/basse-types';
+import converterMap from '../../shared/models/translit-map';
 import Randomizer from './randomizer';
 
 export default class GarageService {
@@ -10,5 +11,18 @@ export default class GarageService {
 
   public generateCar(): Car {
     return { id: '0', name: this.randomizer.getRandomName(), color: this.randomizer.getRandomColor() };
+  }
+
+  public static translitCarName(word: string): string {
+    let answer = '';
+
+    for (let i = 0; i < word.length; ++i) {
+      if (converterMap[word[i]] == undefined) {
+        answer += word[i];
+      } else {
+        answer += converterMap[word[i]];
+      }
+    }
+    return answer;
   }
 }
