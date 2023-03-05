73ac1cb60de585f1a739feb9c50aad0a11323af2
EXisTAnZ <ozim_ssspam@mail.ru>
2023-03-05 13:04:47 +0300

    fix: change api url

--- migration-ts/src/components/controller/appLoader.ts
+++ migration-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.up.railway.app/', {
+        super('https://newsapi.org/v2/', {
             apiKey: 'c58a9286cea842be8894eb94d7e1736d', // получите свой ключ https://newsapi.org/
         });
     }
